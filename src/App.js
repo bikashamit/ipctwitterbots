b618716006ac551fb61cb0d79de7a6de56b14ff9
amit kumar <amit.gangaur@gmail.com>
2023-08-13 21:29:25 +0530

    more corrections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [response, setResponse] = useState('');
+
+  const handleButtonClick = async () => {
+    try {
+      const res = await axios.get('/api/mentionBot');
+      setResponse(res.data.message);
+    } catch (error) {
+      console.error('Error:', error);
+      setResponse('An error occurred');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleButtonClick}>Trigger Bot</button>
+      <p>{response}</p>
     </div>
   );
 }
