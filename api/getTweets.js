fc5fb19379d7984fe77dda14ddad29674a20092b
amit kumar <amit.gangaur@gmail.com>
2023-08-19 22:02:18 +0530

    http req res

--- api/getTweets.js
+++ api/getTweets.js
@@ -8,13 +8,13 @@ module.exports = async (req, res) => {
     if (response.ok) {
       const data = await response.json();
       // Process the Twitter API response here
-      res.status(200).json({ message: 'Twitter data fetched successfully', data });
+      return res.status(200).json({ message: 'Twitter data fetched successfully', data });
     } else {
       console.error('Error fetching Twitter data:', response.statusText);
-      res.status(500).json({ error: 'An error occurred while fetching Twitter data' });
+      return res.status(500).json({ error: 'An error occurred while fetching Twitter data' });
     }
   } catch (error) {
     console.error('Error fetching Twitter data:', error);
-    res.status(500).json({ error: 'An error occurred while fetching Twitter data' });
+    return res.status(500).json({ error: 'An error occurred while fetching Twitter data' });
   }
 };
